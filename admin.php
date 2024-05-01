b2616252aad0d531b34c7e2d08866b0471c35227
faradits <faraditysa@gmail.com>
2024-05-01 15:33:06 +0700

    update by dita

---
+++ admin.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include("inc_koneksi.php");
+if(!isset($_SESSION['admin_username'])) {
+    header("location:login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="app">
+        <nav>
+            <ul>
+                <li><a href="admin.php">Dashboard</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </nav>
+<h1>Selamat Datang!</h1>
+Dashboard
---
+++ login.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+if(isset($_SESSION['admin_username'])) {
+    header("location:admin.php");
+}
+include("inc_koneksi.php");
+$username = "";
+$password = "";
+$err = "";
+if(isset($_POST['login'])) {
+    $username   = $_POST['username'];
+    $password   = $_POST['password'];
+    if  ($username == '' or $password == ''){
+        $err .= "Silahkan Masukkan Username dan Password";
+    }
+    if(empty($err)){
+        $sql1 = "select * from admin where username = '$username'";
+        $q1 = mysqli_query($koneksi, $sql1);
+        $r1 = mysqli_fetch_array($q1);
+        if($r1['password'] != md5($password)) {
+            $err .= "Akun tidak ditemukan";
+        }
+    }
+    if(empty($err)){
+        $_SESSION['admin_username'] = $username;
+        header("location:admin.php");
+        exit();
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="app">
+        <h1>Login</h1>
+        <?php
+        if($err){
+            echo "$err";
+        }
+        ?>
+        <form action="" method="post">
+            <input type="text" value="<?php echo $username ?>" name="username" class="input" placeholder="Masukkan Username" /><br><br>
+            <input type="password" name="password" class="input" placeholder="Masukkan Password" /><br><br>        
+            <input type="submit" name="login" value="Login" />
+        </form>
+    </div>
+</body>
+</html>
