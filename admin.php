738cb7b881567066e7763f464cf81115a0c4592e
Zephya71 <fathoniadi8@gmail.com>
2024-05-06 21:10:03 +0700

    upfate eror

--- admin.php
+++ admin.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['admin_username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Fundraising Desa</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap" />
-    <link rel="stylesheet" href="style_login.css" />
+    <link rel="stylesheet" href="style.css" />
 </head>
 
 <body>
